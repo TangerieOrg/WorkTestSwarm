60f2c7cc574f80a3ad1276b09d258187dfcc93c8
Tangerie <josh.cavill01@gmail.com>
2023-01-18 13:14:55 +1100

    camera test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,22 @@
 FROM python
 
-RUN pip3 install flask
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    bzip2 \
+    g++ \
+    git \
+    graphviz \
+    libgl1-mesa-glx \
+    libhdf5-dev \
+    openmpi-bin \
+    wget \
+    python3-tk && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
 
 COPY . /app
 
--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, Response
 from flask import jsonify
 import socket
 import os
+import cv2
 
 app = Flask(__name__)
+
+video = cv2.VideoCapture(0)
+
+
+def gen(video):
+    while True:
+        success, image = video.read()
+
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 @app.route('/')
 def index():
@@ -11,6 +27,17 @@ def index():
         "Hostname": socket.gethostname(),
         "video": os.path.exists("/dev/video0")
     })
+
+
+@app.route('/video')
+def video_feed():
+    # Set to global because we refer the video variable on global scope,
+    # Or in other words outside the function
+    global video
+
+    # Return the result on the web
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
