59872319e637b21991fd7aa9a3727395079297fe
Tangerie <josh.cavill01@gmail.com>
2023-01-18 13:20:38 +1100

    a

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ video = cv2.VideoCapture(0)
 
 
 def gen(video):
-    while True:
         success, image = video.read()
 
         ret, jpeg = cv2.imencode('.jpg', image)
 
         frame = jpeg.tobytes()
 
-        yield (b'--frame\r\n'
+        return (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
