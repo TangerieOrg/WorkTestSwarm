7769d18386b5d8da8029c4275c3c285bc95f4e9a
Tangerie <josh.cavill01@gmail.com>
2023-01-18 12:58:09 +1100

    tsting

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import jsonify
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(f'App {socket.gethostname()}')
+    return jsonify({
+        "Hostname": socket.gethostname(),
+        "video": os.path.exists("/dev/video0")
+    })
 
 
 if __name__ == '__main__':
